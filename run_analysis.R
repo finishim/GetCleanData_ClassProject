0d9452206d37ae635a17bf634fa233610bb774be
Nazmi Anik <nazmianik@gmail.com>
2015-07-22 15:00:23 -0400

    Initialize the project.

---
+++ run_analysis.R
@@ -0,0 +1,31 @@
+run_analysis <- function() {
+    
+    ## Start by downloading the dataset
+    
+    #dataset_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+    #if(!file.exists("data")) {
+    #    dir.create("data") # create data folder if it does not already exist
+    #}
+    #download.file(dataset_url, destfile = ".\\data\\projectfiles.zip") 
+    #unzip(".\\data\\projectfiles.zip") # unzip the file
+    
+    ## Merge the training and the test sets to create one data set.
+    
+    # Need to parse the files
+    # Load onto a Data Table
+    # Identify a key
+    # Merge the tables
+    
+    ## Extracts only the measurements on the mean and standard deviation for each measurement. 
+    
+    
+    ## Use descriptive activity names to name the activities in the data set
+    
+    
+    ## Appropriately label the data set with descriptive variable names. 
+    
+    
+    ## From the data set in step 4, create a second, independent tidy data set 
+    ## with the average of each variable for each activity and each subject.
+    
+}
